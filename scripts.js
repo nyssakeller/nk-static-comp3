10ff941f71b1177948b5d7a8ff24dc4c77a6148b
Nyssa Keller <nyssa1@gmail.com>
2017-11-07 19:13:16 -0700

    style changes

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var menu = $('.menu');
+
+menu.on('click', function() {
+    $('.sidebar').toggle();
+} )
+
+var tabMenu = $('.tab-menu');
+    tabMenu.on('click', function() {
+        $('.graph-tabs').toggle();
+    })
+
+
